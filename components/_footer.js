baf681fcfdfa4f467e621a643bec1e53f5f21eea
widelachie <me@lachlankemp.com>
2021-03-20 07:20:03 -0700

    design revamp

---
+++ components/_footer.js
@@ -0,0 +1,13 @@
+
+function Footer() {
+    return (
+        <footer style={{textAlign:"center", padding:"1rem"}}>
+            <div>
+                <span>© Accelor 2019 - 2021</span>
+            </div>
+        </footer>
+    )
+}
+  
+export default Footer
+  
