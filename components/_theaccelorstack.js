52abaeb121cfee69eac33c7f17013640b0907b18
widelachie <me@lachlankemp.com>
2021-04-06 16:16:41 +0930

    added liveatspace

--- components/_theaccelorstack.js
+++ components/_theaccelorstack.js
@@ -4,11 +4,11 @@ function Stack() {
         <div style={{marginTop:"2rem", width:"100%"}}>  
             <h2>Technology we use</h2>
             <div className={styles.stack}>
-                <img src="/home/stack/nextjs.svg" alt="NextJS Logo"/>
-                <img src="/home/stack/nodejs.svg" alt="NodeJS Logo"/>
-                <img src="/home/stack/do.svg" alt="DigitalOcean Logo"/>
-                <img src="/home/stack/algolia.svg" alt="Algolia Logo"/>
-                <img src="/home/stack/strapi.svg" alt="Strapi Logo"/>
+                <img src="/home/stack/nextjs.svg" alt="NextJS Logo" className={styles.image} draggable="false" />
+                <img src="/home/stack/nodejs.svg" alt="NodeJS Logo" className={styles.image} draggable="false" />
+                <img src="/home/stack/do.svg" alt="DigitalOcean Logo" className={styles.image} draggable="false" />
+                <img src="/home/stack/algolia.svg" alt="Algolia Logo" className={styles.image} draggable="false" />
+                <img src="/home/stack/strapi.svg" alt="Strapi Logo" className={styles.image} draggable="false" />
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Stack from '../components/_theaccelorstack'
-import Footer from '../components/_footer'
+import Footer from '../components/_footer'  
 
 export default function Home() {
-  var backgrounds = ["/home/homeback-feb2021-1.jpg","/home/homeback-feb2021-2.jpg"]
-  const randomElement = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   
   return (
-    <div className={styles.backgroundwrapper} style={{backgroundImage: `url(${randomElement})`, minHeight: "100vh", backgroundSize:"cover", backgroundRepeat:"no-repeat", backgroundAttachment:"fixed"}}>
+    <div className={styles.backgroundwrapper} style={{backgroundImage: `url('/home/homeback-feb2021-1.jpg')`, minHeight: "100vh", backgroundSize:"cover", backgroundRepeat:"no-repeat", backgroundAttachment:"fixed"}}>
        <div className="container">
       <Head>
         <title>Accelor</title>
@@ -20,13 +18,16 @@ export default function Home() {
           The Web Speed Experts
         </h1>
         
-        <h2>We're the humans that build AccelorEdge™ and other secret projects.</h2>
+        <h2>We're the humans that build Live@Space™ and other secret projects.</h2>
         <div className={styles.grid}>
+          <a href="https://liveatspace.com/" target="_blank" className={styles.card}>
+            <h3>Live@Space™</h3>
+            <p>A live space launch and space activity tracker.</p>
+          </a>
           <a className={styles.card} target="_blank" style={{cursor:"not-allowed"}}>
             <h3>AccelorEdge™ &rarr;</h3>
             <p>A revolutionary new MultiCDN service.</p>
           </a>
-
           <a href="mailto:us@accelor.io" className={styles.card}>
             <h3>Got an Idea? &rarr;</h3>
             <p>Shoot us an email and we'll check it out.</p>
